76d90f9a2aa7433780bc990bc49fb0be988bb115
Christine Cua <cloudycua@gmail.com>
2018-05-31 14:18:40 +0000

    automatically backed up by learn

--- config/environment.rb
+++ config/environment.rb
@@ -15,7 +15,7 @@ Bundler.require
 # put the code to connect to the database here
 # Uses the establish_connection method from ActiveRecord::Base
 # to connect to the artists database
-#which will be created in the migration via SQLite3 (the adapter)
+# which will be created in the migration via SQLite3 (the adapter)
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/artists.sqlite"
